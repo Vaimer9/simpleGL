fd05e1a75bc6957ab3ed562ce36648d8d1a733ab
v9 <v9@DESKTOP-GA3ULSJ.localdomain>
2022-08-24 00:23:17 +0530

    Finished Tutorial #2

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
 #include <iostream>
+#include <homemade_utils.hpp>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
@@ -22,31 +22,60 @@ int main()
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
-
     // glew initialization
     if (glewInit() != GLEW_OK)
     {
         std::cout << "Error!" << std::endl;
         return -1;
     }
-    std::cout << "openGL version: " << glGetString(GL_VERSION) << std::endl;
-
 
     glViewport(0, 0, 800, 600);
     // update gl viewport when glfw window resizes
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    while(!glfwWindowShouldClose(window))
-    {
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+    static const GLfloat g_vertex_matrix[] = {
+       -1.0f, -1.0f, 0.0f,
+       1.0f, -1.0f, 0.0f,
+       0.0f,  1.0f, 0.0f,
+    };
+
+    // Shaders
+    GLuint programID = LoadShaders("shaders/vertex.vert", "shaders/fragment.frag");
+
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_matrix), g_vertex_matrix, GL_STATIC_DRAW);
+
+
+    // Main Loop
+    do {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(programID);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+            0,
+            3,
+            GL_FLOAT,
+            GL_FALSE,
+            0,
+            (void*)0
+        );
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDisableVertexAttribArray(0);
+    
         glfwSwapBuffers(window);
         glfwPollEvents();
-    }
+    } while (!glfwWindowShouldClose(window));
 
     glfwTerminate();
     return 0;
 }
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
