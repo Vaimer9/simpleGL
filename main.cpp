aeaa0a1b3f007bb0ca7e520eab497194b66ae56b
v9 <v9@DESKTOP-GA3ULSJ.localdomain>
2022-08-23 23:09:46 +0530

    init

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+int main()
+{
+    // glfw initialization and window creation
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Amongus sussy baka lmao?", nullptr, nullptr);
+    if (!window)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+
+    // glew initialization
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Error!" << std::endl;
+        return -1;
+    }
+    std::cout << "openGL version: " << glGetString(GL_VERSION) << std::endl;
+
+
+    glViewport(0, 0, 800, 600);
+    // update gl viewport when glfw window resizes
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
